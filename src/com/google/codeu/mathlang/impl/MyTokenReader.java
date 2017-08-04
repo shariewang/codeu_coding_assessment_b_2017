a1ed79eb7f28ef483137022730f77aa44b0335fa
Sharie Wang <Sharie Wang>
2017-08-04 00:20:04 -0700

    pass all tests

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -14,10 +14,13 @@
 
 package com.google.codeu.mathlang.impl;
 
-import java.io.IOException;
-
-import com.google.codeu.mathlang.core.tokens.Token;
+import com.google.codeu.mathlang.core.tokens.*;
 import com.google.codeu.mathlang.parsing.TokenReader;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 // MY TOKEN READER
 //
@@ -27,9 +30,17 @@ import com.google.codeu.mathlang.parsing.TokenReader;
 // work with the test of the system.
 public final class MyTokenReader implements TokenReader {
 
+  private String source;
+  private StringBuilder builder;
+  private int curr;
+  private static final Set<String> SYMBOLS = new HashSet<>(Arrays.asList("-", "=", "+"));
+
   public MyTokenReader(String source) {
     // Your token reader will only be given a string for input. The string will
     // contain the whole source (0 or more lines).
+    this.source = source;
+    this.curr = 0;
+    this.builder = new StringBuilder();
   }
 
   @Override
@@ -41,6 +52,71 @@ public final class MyTokenReader implements TokenReader {
     // If for any reason you detect an error in the input, you may throw an IOException
     // which will stop all execution.
 
-    return null;
+    //skip all leading whitespace
+    while (remaining() > 0 && Character.isWhitespace(peek())) {
+      read();
+    }
+    if (remaining() <= 0) {
+      return null;
+    } else if (peek() == ';') {
+      return new SymbolToken(read());
+    } else if (peek() == '"') {
+      return readQuotedToken();
+    } else {
+      return readNonQuotedToken();
+    }
+  }
+
+  private int remaining() {
+    return source.length() - curr;
+  }
+
+  private char peek() throws IOException {
+    if (curr < source.length()) {
+      return source.charAt(curr);
+    } else {
+      throw new IOException("No more characters");
+    }
+  }
+
+  private char read() throws IOException {
+    curr++;
+    return peek();
+  }
+
+  private Token readQuotedToken() throws IOException {
+    builder.setLength(0); //clear
+    read(); // opening quote
+    while (peek() != '"') {
+      builder.append(read());
+    }
+    read(); // closing quote
+    return new StringToken(builder.toString());
+  }
+
+  private Token readNonQuotedToken() throws IOException {
+    builder.setLength(0); //clear
+
+    while(peek() != ' ') {
+      builder.append(read());
+      // handling cases in which no more should be added to current token
+      char next = peek();
+      if (next == ';' || SYMBOLS.contains(Character.toString(next)) || Character.isDigit(next)) {
+        break;
+      }
+    }
+
+    String word = builder.toString();
+    try {
+      return new NumberToken(Double.parseDouble(word));
+    } catch (Exception ignored) {}
+
+    if (SYMBOLS.contains(word)) {
+      return new SymbolToken(word.charAt(0));
+    } else if (word.charAt(0) >= 65 && word.charAt(0) <= 122) {
+      return new NameToken(word);
+    } else {
+      throw new IOException("Invalid input");
+    }
   }
 }
